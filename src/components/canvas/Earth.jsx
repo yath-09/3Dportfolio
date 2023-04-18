f0b8785f3ae5e77fee866e229a282c67fc82d71e
Yatharth <yatharth0903@gmail.com>
2023-04-19 00:17:45 +0530

    angle changed for earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -30,8 +30,8 @@ const EarthCanvas = () => {
         <OrbitControls
           autoRotate
           enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI}
+          minPolarAngle={Math.PI/2}
         />
         <Earth />
 
